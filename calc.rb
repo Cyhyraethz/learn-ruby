5145b0b366320ed05c72c28da0fbcb45a7783bf8
Dylan Premo <maboose@yahoo.com>
2020-03-16 14:03:53 -0700

    Fill out first chapter exercises

--- calc.rb
+++ calc.rb
@@ -1,1 +1,7 @@
-puts 1+2
+puts "There are #{24 * 365} hours in a year."
+puts "There are #{60 * 24 * 365 * 10} minutes in a decade."
+puts "I am #{29.726 * 365 * 24 * 60 * 60} seconds old."
+puts "I hope to eat #{0 * 365 * 100} chocolates in my life."
+
+puts "Question: If I am 1298 million seconds old, how many years old am I?"
+puts "Answer: #{(1298000000) / 60 / 60 / 24 / 365} years old."

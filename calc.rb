5155970d150e6bce06d5eb17cc7bec26ba90a11b
Dylan Premo <maboose@yahoo.com>
2020-03-16 14:37:28 -0700

    Complete next few chapters

--- calc.rb
+++ calc.rb
@@ -1,7 +1,27 @@
-puts "There are #{24 * 365} hours in a year."
-puts "There are #{60 * 24 * 365 * 10} minutes in a decade."
-puts "I am #{29.726 * 365 * 24 * 60 * 60} seconds old."
-puts "I hope to eat #{0 * 365 * 100} chocolates in my life."
+# puts "There are #{24 * 365} hours in a year."
+# puts "There are #{60 * 24 * 365 * 10} minutes in a decade."
+# puts "I am #{29.726 * 365 * 24 * 60 * 60} seconds old."
+# puts "I hope to eat #{0 * 365 * 100} chocolates in my life."
 
-puts "Question: If I am 1298 million seconds old, how many years old am I?"
-puts "Answer: #{(1298000000) / 60 / 60 / 24 / 365} years old."
+# puts "Question: If I am 1298 million seconds old, how many years old am I?"
+# puts "Answer: #{(1298000000) / 60 / 60 / 24 / 365} years old."
+
+# puts 'Hello there, and what\'s your name?'
+# name = gets.chomp
+# puts 'Your name is ' + name + '? What a lovely name!'
+# puts 'Pleased to meet you, ' + name + '. :)'
+
+# puts 'What is your first name?'
+# first_name = gets.chomp
+# puts 'What is your middle name?'
+# middle_name = gets.chomp
+# puts 'What is your last name?'
+# last_name = gets.chomp
+# puts "Pleased to meet you, #{first_name} #{middle_name} #{last_name}!"
+
+# puts 'What is your favorite number?'
+# favorite_number = gets.chomp
+# puts "May I suggest a bigger and better new favorite number, such as #{favorite_number.to_i + 1}?"
+
+# puts 'The methods we have seen so far include 1: "puts", 2: "gets", 3: c"homp", 4: "to_i", 5: "to_s", 6: "to_f", 7: "+", 8: "-", 9: "*", and 10: "/"'
+
